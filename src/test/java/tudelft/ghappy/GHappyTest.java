7a30a3043690d13eb5764f1f2aef8bc4a90878db
edvm <edvm.dev@gmail.com>
2021-05-08 00:50:36 -0400

    test of GHappy exercise

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,28 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.mirror.Mirror;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "{0}({1}, {2})")
+    @CsvSource({
+        // Happy Words
+        "'happy-word',xxggxx,true", "'happy-word',xggxggxx,true", "'happy-word',ggg,true",
+    })
+    public void testAlgorithmTrue(String partition, String word, String expectedResult) {
+        Boolean result = new GHappy().gHappy(word);
+        Assertions.assertTrue(result);
+    }
+
+    @ParameterizedTest(name = "{0}({1}, {2})")
+    @CsvSource({
+        // Happy Words
+        "'happy-word',xxgxx,false", "'happy-word',xxggyygxx,false", "'happy-word',g,false",
+    })
+    public void testAlgorithmFalse(String partition, String word, String expectedResult) {
+        Boolean result = new GHappy().gHappy(word);
+        Assertions.assertFalse(result);
+    }
 }
